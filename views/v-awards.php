a3f68a70a72dc0e912348a3a6de1f2a98428156c
RhodAyo <aroluwole@gmail.com>
2022-02-01 04:54:53 +0000

    award

--- views/v-awards.php
+++ views/v-awards.php
@@ -25,25 +25,168 @@
                     </div>
                  </div>
                 <div class="s18-age-event l-info-pack-days">
-                    <ul>
+                    <div class="tour_head1 udb-time-line days">
+                                <ul>
+                                    <li class="l-info-pack-plac"> <i class="fa fa-clock-o" aria-hidden="true"></i>
+                                        <div class="sdb-cl-tim">
+                                            <div class="sdb-cl-day">
+                                                <h5>First week</h5>
+                                                <span>07 February 2022</span>
+                                            </div>
+                                            <div class="sdb-cl-class">
+                                                <ul>
+                                                    <li>
+                                                        <!-- <div class="sdb-cl-class-tim">
+                                                            <span>9:30 am</span>
+                                                            <span>10:15 am</span>
+                                                        </div> -->
+                                                        <div class="sdb-cl-class-name">
+                                                            <h5>Commencement of Registration for fresh and returning students <span>Folorunsho Ladoke</span></h5>
+                                                            <span class="sdn-hall-na">School Hall 1</span>
+                                                        </div>
+                                                    </li>
+                                                    <li>
+                                                        <!-- <div class="sdb-cl-class-tim">
+                                                            <span>10:15 am</span>
+                                                            <span>11:00 am</span>
+                                                        </div> -->
+                                                        <div class="sdb-cl-class-name">
+                                                            <h5>Orientation for New Students <span>Gbenga Joseph, VP</span></h5>
+                                                            <span class="sdn-hall-na">School Hall 1 & 2</span>
+                                                        </div>
+                                                    </li>
+                                                    <!-- <li>
+                                                        <div class="sdb-cl-class-tim">
+                                                            <span>11:00 am</span>
+                                                            <span>11:45 am</span>
+                                                        </div>
+                                                        <div class="sdb-cl-class-name sdb-cl-class-name-lev">
+                                                            <h5>Board Exam Training Classes <span>Matthew</span></h5>
+                                                            <span class="sdn-hall-na">Apj Hall 112</span>
+                                                        </div>
+                                                    </li> -->
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="l-info-pack-plac"> <i class="fa fa-clock-o" aria-hidden="true"></i>
+                                        <div class="sdb-cl-tim">
+                                            <div class="sdb-cl-day">
+                                                <h5>13 weeks Teaching/Revision.</h5>
+                                                <span>14 February, 2022 - 06 May, 2022</span>
+                                            </div>
+                                            <div class="sdb-cl-class">
+                                                <ul>
+                                                    <li>
+                                                        <div class="sdb-cl-class-tim">
+                                                            <span>14 March - 18 March, 2022</span>
+                                                            <!-- <span>10:15 am</span> -->
+                                                        </div>
+                                                        <div class="sdb-cl-class-name">
+                                                            <h5>First CA Test
+                                                                <!-- <span>John Smith</span> -->
+                                                            </h5>
+                                                            <!-- <span class="sdn-hall-na">Apj Hall 112</span> -->
+                                                        </div>
+                                                    </li>
+                                                    <li>
+                                                        <div class="sdb-cl-class-tim">
+                                                            <span>04 April, 2022 - 08 April, 2022.</span>
+                                                            <!-- <span>11:00 am</span> -->
+                                                        </div>
+                                                        <div class="sdb-cl-class-name">
+                                                            <h5>Second CA Test 
+                                                                <!-- <span>Stephanie</span> -->
+                                                            </h5>
+                                                            <!-- <span class="sdn-hall-na">Apj Hall 112</span> -->
+                                                        </div>
+                                                    </li>
+                                                    <li>
+                                                        <!-- <div class="sdb-cl-class-tim">
+                                                            <span>11:00 am</span>
+                                                            <span>11:45 am</span>
+                                                        </div> -->
+                                                        <!-- <div class="sdb-cl-class-name sdb-cl-class-name-lev">
+                                                            <h5>Web Design & Development <span>Matthew</span></h5>
+                                                            <span class="sdn-hall-na">Apj Hall 112</span>
+                                                        </div> -->
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="l-info-pack-plac"> <i class="fa fa-clock-o" aria-hidden="true"></i>
+                                        <div class="sdb-cl-tim">
+                                            <div class="sdb-cl-day">
+                                                <h5>Final Examination</h5>
+                                                <span>16 May, 2022 - 27 May, 2022.</span>
+                                            </div>
+                                            <div class="sdb-cl-class">
+                                                <ul>
+                                                    <li>
+                                                        <div class="sdb-cl-class-tim">
+                                                            <span>16 May, 2022 - 20 May, 2022</span>
+                                                            <!-- <span>10:15 am</span> -->
+                                                        </div>
+                                                        <div class="sdb-cl-class-name">
+                                                            <h5>Junior Secondary Examinations.
+                                                                <!-- <span>John Smith<span> -->
+                                                            </h5>
+                                                            <!-- <span class="sdn-hall-na">Apj Hall 112</span> -->
+                                                        </div>
+                                                    </li>
+                                                    <li>
+                                                        <div class="sdb-cl-class-tim">
+                                                            <span>23 May, 2022 - 27 May, 2022</span>
+                                                            <!-- <span>11:00 am</span> -->
+                                                        </div>
+                                                        <div class="sdb-cl-class-name">
+                                                            <h5>Senior Secondary Examinations.         
+                                                                <!-- <span>Stephanie</span> -->
+                                                        </h5>
+                                                            <!-- <span class="sdn-hall-na">Apj Hall 112</span> -->
+                                                        </div>
+                                                    </li>
+                                                    <!-- <li>
+                                                        <div class="sdb-cl-class-tim">
+                                                            <span>11:00 am</span>
+                                                            <span>11:45 am</span>
+                                                        </div>
+                                                        <div class="sdb-cl-class-name sdb-cl-class-name-lev">
+                                                            <h5>Board Exam Training Classes <span>Matthew</span></h5>
+                                                            <span class="sdn-hall-na">Apj Hall 112</span>
+                                                        </div>
+                                                    </li> -->
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="l-info-pack-plac"> <i class="fa fa-clock-o" aria-hidden="true"></i>
+                                        <h4><span>Holiday: </span> Childrens Day </h4>
+                                        <p>Childrens Day: National Holiday.</p>
+                                        <p>School Vacation: 30th May, 2022.</p>
+                                    </li>
+                                </ul>
+                            </div>
+                    <!-- <ul>
 
-                        <?php while($award = mysqli_fetch_assoc($awards)){ ?>
+                        <?php //while($award = mysqli_fetch_assoc($awards)){ ?>
                         <li>
                             <div class="age-eve-com age-eve-1">
                                 <img src="images/icon/awa/2.png" alt="">
                             </div>
                             <div class="s17-eve-time">
                                 <div class="s17-eve-time-tim">
-                                    <span><?php echo $award['time']; ?></span>
+                                    <span><?php //echo $award['time']; ?></span>
                                 </div>
                                 <div class="s17-eve-time-msg">
-                                    <h4><?php echo $award['name']; ?></h4>
-                                    <p><?php echo $award['description']; ?></p>
-                                    <?php if(!$award['toggle1_title'] == NULL && !$award['toggle1_des'] == NULL){ ?>
+                                    <h4><?php //echo $award['name']; ?></h4>
+                                    <p><?php //echo $award['description']; ?></p>
+                                    <?php //if(!$award['toggle1_title'] == NULL && !$award['toggle1_des'] == NULL){ ?>
 
                                     <div class="time-hide time-hide-1">
-                                        <h5><?php echo $award['toggle1_title']; ?></h5>
-                                        <p><?php echo $award['toggle1_des']; ?></p>
+                                        <h5><?php //echo $award['toggle1_title']; ?></h5>
+                                        <p><?php //echo $award['toggle1_des']; ?></p>
 
                                     </div>
                                     <a href="#!" class="s17-sprit age-dwarr-btn time-hide-1-btn">
@@ -52,11 +195,11 @@
                                     <a href="#!" class="s17-sprit age-dwarr-btn time-hide-11-btn hb-com">
                                         <i class="fa fa-angle-up"></i>
                                     </a>
-                                        <?php if(!$award['toggle2_title'] == NULL && !$award['toggle2_des'] == NULL){ ?>
+                                        <?php //if(!$award['toggle2_title'] == NULL && !$award['toggle2_des'] == NULL){ ?>
 
                                         <div class="time-hide time-hide-1">
-                                            <h5><?php echo $award['toggle2_title']; ?></h5>
-                                            <p><?php echo $award['toggle2_des']; ?></p>
+                                            <h5><?php //echo $award['toggle2_title']; ?></h5>
+                                            <p><?php //echo $award['toggle2_des']; ?></p>
 
                                         </div>
                                         <a href="#!" class="s17-sprit age-dwarr-btn time-hide-1-btn">
@@ -76,7 +219,7 @@
 
 
 
-                    </ul>
+                    </ul> -->
                 </div>
                 <div class="ed-about-sec1">
                     <div class="col-md-6"></div>
