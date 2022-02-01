fd3e4d34c62482d6ed9f93f92bdb2ba55753c531
ibiyemialake <98369940+ibiyemialake@users.noreply.github.com>
2022-02-01 08:50:15 +0100

    Update v-admission.php

--- views/v-admission.php
+++ views/v-admission.php
@@ -1,9 +1,9 @@
 <?php 
-// $classSQL = "SELECT * FROM `class`";
-// $classes = $crud->select($classSQL);
+$classSQL = "SELECT * FROM `class`";
+$classes = $crud->select($classSQL);
 
-//  $courseReq = "SELECT * FROM `courses`";
-//  $courses =  $crud->select($courseReq);
+ $courseReq = "SELECT * FROM `courses`";
+ $courses =  $crud->select($courseReq);
 
  if(isset($_POST['admission_request'])){
      extract($_POST);
@@ -86,21 +86,21 @@
                                 <input type="text" class="form-control" placeholder="Enter your city" name="req_admsn_city">
                             </div>
                         </div>
-                        <!-- <div class="form-group">
+                         <div class="form-group">
                             <label class="control-label col-sm-3">Education:</label>
                             <div class="col-sm-9">
                                 <input type="text" class="form-control" placeholder="Enter your education" name="req_admsn_education">
                             </div>
-                        </div> -->
-<!--                         <div class="form-group">
+                        </div>
+                       <div class="form-group">
                             <label class="control-label col-sm-3">Class:</label>
                             <div class="col-sm-9">
                                 <select name="req_admsn_course">
                                     <option value="" selected disabled>-- Select Class --</option>
-                                    <?php //while($course = mysqli_fetch_assoc($classes)){ ?>
-                                        <option value="<?php// echo $course['name']; ?>"><?php //echo $course['name']; ?></option>
+                                    <?php while($course = mysqli_fetch_assoc($classes)){ ?>
+                                        <option value="<?php echo $course['name']; ?>"><?php echo $course['name']; ?></option>
                                     <?php } ?>
-                                    <!-- <option>Aerospace Engineering</option>
+                                    <option>Aerospace Engineering</option>
                                     <option>Agriculture Courses</option>
                                     <option>Fashion Technology</option>
                                     <option>Marine Engineering</option>
@@ -109,10 +109,10 @@
                                     <option>Accountant course</option>
                                     <option>Dot Net Development</option>
                                     <option>Java Development</option>
-                                    <option>Chemical Engineering</option> -->
+                                    <option>Chemical Engineering</option> 
                                 </select>
                             </div>
-                        </div> -->
+                        </div> 
                         <div class="form-group mar-bot-0">
                             <div class="col-sm-offset-3 col-sm-9">
                                 <i class="waves-effect waves-light light-btn waves-input-wrapper"><input type="submit" value="APPLY NOW" class="waves-button-input" name="admission_request"></i>
