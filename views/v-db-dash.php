e6c9c418d0d6dbaa7c4334ebfd7fa3e1036576ff
RhodAyo <aroluwole@gmail.com>
2022-02-01 09:22:04 +0000

    userdash

--- views/v-db-dash.php
+++ views/v-db-dash.php
@@ -21,7 +21,7 @@ if(isset($_GET['status'])){
 
 ?>
 <!--== User Details ==-->
-<h2>All Exam List</h2>
+<!-- <h2>All Exam List</h2>
 <div class="sb2-2-3">
     <div class="row">
         <div class="col-md-12">
@@ -61,4 +61,4 @@ if(isset($_GET['status'])){
             </div>
         </div>
     </div>
-</div>
+</div> -->
